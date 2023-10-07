583ec2931a453627642ee90812ab31f79775deda
Amdres <japovea@uninorte.edu.co>
2023-10-07 16:36:31 -0500

    I created a .gitignore file to finally resolve the node_modules tracking. I also changed the path in the socket connection

--- src/socket.js
+++ src/socket.js
@@ -1,6 +1,6 @@
 import {io} from 'socket.io-client'
 const socket = io('http://localhost:3000', {
-    path: '/socket.io/',
-    transports: ['websocket', 'polling']
+    path: '/api/',
+    transports: ['websocket','polling']
 })
 export default socket
