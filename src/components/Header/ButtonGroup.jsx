0f2eaf2cbdb2cee3684c3e8e7b31508616b184c4
Amdres <japovea@uninorte.edu.co>
2023-10-07 14:40:38 -0500

    I deleted some unused dependencies. Finally, i solved the problem in firefox with the websocket. It seems that the transport method was the problem so now socket.io can use both polling and websocket protocols to establish the connection in case any of them dont work. At this point, the socket should be detected in most browsers :)

--- src/components/Header/ButtonGroup.jsx
+++ src/components/Header/ButtonGroup.jsx
@@ -1,37 +1,17 @@
 import React, { useEffect } from "react";
 import { Button, Stack } from "@chakra-ui/react";
-import io from "socket.io-client";
+import socket from "../../socket.js";
 
 const ButtonGroup = () => {
-  const socket = io('http://localhost:3000', { transports: ['websocket'] });
-
   const handleFindArduinoClick = () => {
-    console.log("me cago en tus muertos")
-    socket.on('connect', () => {
-      console.log('Connected to server');
+    // Emit the 'findArduino' event to the server
+    socket.emit("findArduino");
+
+    // Listen for the 'arduinoFound' event from the server
+    socket.on("findArduino", (data) => {
+      alert(data.message);
     });
-    socket.on('connect_error', (error) => {
-      console.error('Connection error:', error);
-    });
-    
-
-
-
-    socket.emit('findArduino');
   };
-
-  useEffect(() => {
-    // Listen for the 'findArduino' event response from the server
-    socket.on('findArduino', (res) => {
-      // Log the server response to the browser console
-      console.log(res.message); // Assuming the response contains a 'message' property
-    });
-
-    // Clean up the event listener when the component unmounts
-    return () => {
-      socket.off('findArduino');
-    };
-  }, []);
 
   return (
     <Stack direction="row" spacing={4}>
