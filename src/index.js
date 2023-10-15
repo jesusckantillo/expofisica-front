c14367711a663ca5eaffd58d5204359094a0a751
Sebastian <carsebastian1013@gmail.com>
2023-10-15 13:39:40 -0500

    version 0.0.6

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ ReactDOM.render(
 				<HashRouter>
 					<Switch>
 						<Route path={`/admin`} component={AdminLayout} />
-						<Redirect from='/' to='/admin' />
+						<Redirect from='/' to='/admin/main-dashboard' />
 					</Switch>
 				</HashRouter>
 			</ThemeEditorProvider>
