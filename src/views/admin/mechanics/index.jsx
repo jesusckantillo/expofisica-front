5a9eec1a92269e9d9419fa3b42bcfa81d2feacaa
Sebastian <carsebastian1013@gmail.com>
2023-10-19 09:46:57 -0500

    version 0.2.0

--- src/views/admin/mechanics/index.jsx
+++ src/views/admin/mechanics/index.jsx
@@ -1,5 +1,6 @@
 
 import React from "react";
+import { useState } from "react";
 
 // Chakra imports
 import {
@@ -12,21 +13,90 @@ import {
   useColorModeValue,
   SimpleGrid,
 } from "@chakra-ui/react";
+import { ConfigProvider, Table } from "antd";
 import Information from "../electricity/components/Information";
 import { experience1 } from "variables/information";
 import { ArrowForwardIcon } from "@chakra-ui/icons";
 
 
 import TotalSpent from "../default/components/TotalSpent";
+import { Components } from "antd/es/date-picker/generatePicker";
 
 export default function Marketplace() {
 
+  const tableStyles = {
+    border: '1px solid #e8e8e8',
+    borderRadius: '20px',
+    FontFamily: 'Plus Jakarta Sans'
+  };
+
+  const columns = [
+    {
+      title: 'Nombre',
+      dataIndex: 'name',
+      key: 'name',
+      render: text => <Link>{text}</Link>,
+    },
+    {
+      title: 'Descripción',
+      dataIndex: 'description',
+      key: 'description',
+    },
+    {
+      title: 'Acción',
+      dataIndex: 'action',
+      key: 'action',
+      render: () => <Button colorScheme="blue" variant="solid" size="sm"><ArrowForwardIcon /></Button>,
+    },
+  ];
+
+  const tableData = [
+    {
+      key: '1',
+      name: 'Experimento 1',
+      description: 'Experimento de física 1',
+    },
+    {
+      key: '2',
+      name: 'Experimento 2',
+      description: 'Experimento de física 2',
+    },
+    {
+      key: '3',
+      name: 'Experimento 3',
+      description: 'Experimento de física 3',
+    },
+  ];
+
   const [isVerified, setIsVerified] = React.useState(false);
   const [experimentFinished, setExperimentFinished] = React.useState(false);
+  const [data, setData] = useState([]);
   // Chakra Color Mode
   const textColor = useColorModeValue("secondaryGray.900", "white");
   const textColorBrand = useColorModeValue("brand.500", "white");
   return (
+    <ConfigProvider
+    theme={{
+      components: {
+        Button: {
+          colorPrimary: '#195aec',
+          borderRadius: '20px',
+          algorithm: true,
+        },
+        Input: {
+          colorPrimary: '#195aec',
+          borderRadius: '20px',
+          algorithm: true,
+        },
+        Table: {
+          fontFamily: 'Plus Jakarta Sans',
+        },
+        Select: {
+          borderRadius: '20px',
+        },
+      },
+    }}
+    >
     <Box pt={{ base: "130px", md: "80px", xl: "80px" }}>
       <div>
         <SimpleGrid marginBottom={4} columns={3} spacing={2}>
@@ -45,9 +115,15 @@ export default function Marketplace() {
         <TotalSpent />
       </SimpleGrid>
       <SimpleGrid columns={{ base: 1, md: 2, xl: 2 }} gap="20px" mb="20px">
+        <Table
+          style={tableStyles}
+          columns={columns}
+          dataSource={tableData}        
+        />
         <Information title = 'Una zona para mostrar valores relevantes del experimento' value = "I can see the future, it's lookin like we level through the sky"/>
         <Information  title = 'Experimento' value = 'I can see the future, I can see the future' />
       </SimpleGrid>
     </Box>
+    </ConfigProvider>
   );
 }
