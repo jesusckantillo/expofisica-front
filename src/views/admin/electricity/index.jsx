f08aa9c0000bff29641d81c6e6f98a41172bbc0f
Sebastian <carsebastian1013@gmail.com>
2023-10-20 03:05:11 -0500

    version 0.2.2

--- src/views/admin/electricity/index.jsx
+++ src/views/admin/electricity/index.jsx
@@ -1,47 +1,111 @@
-
-import { useState, useEffect } from "react";
-
-import { Box, Grid, SimpleGrid } from "@chakra-ui/react";
-
-// Custom components
-import TotalSpent from "views/admin/default/components/TotalSpent";
-import Information from "./components/Information";
-import { Button, Icon } from '@chakra-ui/react'
-
-// Assets
 import React from "react";
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import {
+  Box,
+  Button,
+  Image,
+  Flex,
+  Grid,
+  Text,
+  useColorModeValue,
+  SimpleGrid,
+  Heading,
+} from "@chakra-ui/react";
+import { ConfigProvider, Table } from "antd";
+import Information from "../electricity/components/Information";
+import { experience1 } from "variables/information";
+import { ArrowForwardIcon } from "@chakra-ui/icons";
+import mockup from "components/icons/sirius_mockup2.png";
+import caidLibre from "components/icons/detector.jpg";
+import movimientoParticula from "components/icons/randommachine.jpg";
+import TotalSpent from "../default/components/TotalSpent";
+import { Components } from "antd/es/date-picker/generatePicker";
 
-export default function Overview() {
+export default function Marketplace() {
+  const tableStyles = {
+    border: "1px solid #e8e8e8",
+    borderRadius: "20px",
+    FontFamily: "Plus Jakarta Sans",
+  };
 
+  const [isVerified, setIsVerified] = React.useState(false);
+  const [experimentFinished, setExperimentFinished] = React.useState(false);
   const [data, setData] = useState([]);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
-  const [isVerified, setIsVerified] = useState(false);
-  const [experimentFinished, setExperimentFinished] = useState(false);
-
+  // Chakra Color Mode
+  const textColor = useColorModeValue("secondaryGray.900", "white");
+  const textColorBrand = useColorModeValue("brand.500", "white");
+  const brandColor = useColorModeValue("brand.500", "white");
+  const boxBg = useColorModeValue("secondaryGray.300", "whiteAlpha.100");
 
   return (
-    <Box pt={{ base: "130px", md: "80px", xl: "80px" }}>
-      {/* Main Fields */}
-      <div>
-        <SimpleGrid marginBottom={4} columns={3} spacing={2}>
-          <Button colorScheme="green" variant="solid" size="md">
-            Verificar conexión
-          </Button>
-          <Button disabled={!isVerified} colorScheme="blue" variant="solid" size="md">
-            Iniciar experimento
-          </Button>
-          <Button disabled={!experimentFinished} colorScheme="blue" variant="solid" size="md">
-            Descargar datos
-          </Button>
-        </SimpleGrid>
-      </div>
-      <SimpleGrid columns={{ base: 1, md: 2, xl: 1 }} gap='20px' mb='20px'>
-        <TotalSpent />
-      </SimpleGrid>
-      <SimpleGrid columns={{ base: 1, md: 2, xl: 2 }} gap='20px' mb='20px'>
-        <Information title = 'Experimento' value = 'Este es la información base del experimento de arriba'/>
-      </SimpleGrid>
-    </Box>
+    <ConfigProvider
+      theme={{
+        components: {
+          Button: {
+            colorPrimary: "#195aec",
+            borderRadius: "20px",
+            algorithm: true,
+          },
+          Input: {
+            colorPrimary: "#195aec",
+            borderRadius: "20px",
+            algorithm: true,
+          },
+          Table: {
+            fontFamily: "DM Sans",
+          },
+          Select: {
+            borderRadius: "20px",
+          },
+        },
+      }}
+    >
+      <Box pt={{ base: "130px", md: "80px", xl: "80px" }}>
+        <Box textAlign="center" mb="20px">
+          <Heading as="h1" size="2xl" mb="10px">
+            Selecciona la experiencia
+          </Heading>
+          <Text fontSize="xl" mb="20px">
+            Selecciona el experimento que deseas realizar
+          </Text>
+          <Grid templateColumns="1fr 1fr" gap={4}>
+            {/* Cuadrícula con dos columnas */}
+            <Box display="inline-block" textAlign="center">
+              <Text fontSize="xl" mb="20px" color={brandColor}>
+                Caída libre
+              </Text>
+              <Link to="/admin/mechanics/mechanics-1">
+                <Image
+                  src={caidLibre}
+                  alt="SIRIUS"
+                  maxWidth="45%"
+                  ml="190px"
+                  mb="20px"
+                  borderRadius="20px"
+                  boxShadow="0px 0px 50px #ccc"
+                />
+              </Link>
+            </Box>
+            <Box display="inline-block" textAlign="center">
+              <Text fontSize="xl" mb="20px" color={brandColor}>
+                Movimiento de partícula
+              </Text>
+              <Link to="/admin/mechanics/mechanics-2">
+                <Image
+                  src={movimientoParticula}
+                  alt="SIRIUS"
+                  maxWidth="45%"
+                  ml="190px"
+                  mb="20px"
+                  borderRadius="20px"
+                  boxShadow="0px 0px 50px #ccc"
+                />
+              </Link>
+            </Box>
+          </Grid>
+        </Box>
+      </Box>
+    </ConfigProvider>
   );
 }
