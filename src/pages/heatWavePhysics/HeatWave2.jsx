7fc990eca2a7d5d55dce678312e3bad2c249d2ad
yzhuye <yzhu@uninorte.edu.co>
2023-10-20 02:05:57 -0500

    Add SinWave

--- src/pages/heatWavePhysics/HeatWave2.jsx
+++ src/pages/heatWavePhysics/HeatWave2.jsx
@@ -1,15 +1,15 @@
 import React from "react";
+import SinWave from "./Animation/SinWave";
 
 const HeatWave2 = () => {
-    return (
-        <div>
-        <div>Experimento 1 de fisica mecanica</div>
-        <div>
-        <button>Iniciar Transmisión</button>
-        <button>Pausar Transmisión</button>
+  return (
+    <div>
+      <div>Experimento 1 de fisica mecanica</div>
+      <div>
+        < SinWave frequency={500} />
       </div>
-        </div>
-    )
-}
+    </div>
+  );
+};
 
 export default HeatWave2;
